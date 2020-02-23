c37a401bbf9a8c93267ab94037dbf99e7407d010
anguler <xiaoli.zhu@hiii-life.com>
2020-02-23 21:32:36 +0800

    antd框架删减

---
+++ src/test/src/pages/Admin.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Card, Typography, Alert, Icon } from 'antd';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+
+export default () => (
+  <PageHeaderWrapper content=" 这个页面只有 admin 权限才能查看">
+    <div>测试5</div>
+  </PageHeaderWrapper>
+);
